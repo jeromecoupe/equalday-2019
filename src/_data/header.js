5b0f1cd24ec20c34a34cd38cde78a570a3ba2525
jérôme coupé <jercoupe@gmail.com>
2019-04-25 15:33:30 +0200

    fix event date in header
    Signed-off-by: jérôme coupé <jercoupe@gmail.com>

--- src/_data/header.js
+++ src/_data/header.js
@@ -2,16 +2,16 @@ module.exports = {
   fr: {
     eventName: "EQUAL Day 2019",
     eventTitle: "Droit et|ou Désobéissance Civile",
-    eventData: "27 Octobre 2019, Bruxelles, Flagey"
+    eventData: "17 Octobre 2019, Bruxelles, Flagey"
   },
   nl: {
     eventName: "EQUAL Day 2019",
     eventTitle: "NL - Droit et|ou Désobéissance Civile",
-    eventData: "27 Octobre 2019, Brussels, Flagey"
+    eventData: "17 Octobre 2019, Brussels, Flagey"
   },
   en: {
     eventName: "EQUAL Day 2019",
     eventTitle: "EN - Droit et|ou Désobéissance Civile",
-    eventData: "27 Octobre 2019, Brussels, Flagey"
+    eventData: "17 Octobre 2019, Brussels, Flagey"
   }
 };
