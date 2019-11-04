5e6c0e35aaba60fd213cce41f2fb64263d552724
jérôme coupé <jercoupe@gmail.com>
2019-11-04 13:22:59 +0100

    Archives in FR and CSS simplification

---
+++ src/_data/archives.js
@@ -0,0 +1,21 @@
+module.exports = {
+  active: true,
+  fr: {
+    label: "Archives",
+    url: "/archives/",
+    text:
+      "Voici les reportages graohiques et les vidéos réalisés pendant l'événement. Retrouvons-nous l'année prochaine pour l'EQUAL Day 2020!"
+  },
+  fr: {
+    label: "Archives",
+    url: "/archives/",
+    text:
+      "Voici les reportages graohiques et les vidéos réalisés pendant l'événement. Retrouvons-nous l'année prochaine pour l'EQUAL Day 2020!"
+  },
+  fr: {
+    label: "Archives",
+    url: "/archives/",
+    text:
+      "Voici les reportages graohiques et les vidéos réalisés pendant l'événement. Retrouvons-nous l'année prochaine pour l'EQUAL Day 2020!"
+  }
+};
